96cf62143303280d55becb5ce433263d93329cd7
NIU01558589 <1558589@uab.cat>
2024-10-21 16:56:28 +0200

    cambio

--- src/main/java/org/uab/joclau/battleships/model/Board.java
+++ src/main/java/org/uab/joclau/battleships/model/Board.java
@@ -4,29 +4,29 @@ import java.util.List;
 
 public class Board {
 
-  int [][] board;
-  //Tauler del joc
-  List<Ship> ships;
-  //Tots els vaixells del joc
+    int [][] board;
+    //Tauler del joc
+    List<Ship> ships;
+    //Tots els vaixells del joc
 
-  public void placeShip(Ship ship, int x, int y, boolean isHorizontal){
+    public void placeShip(Ship ship, int x, int y, boolean isHorizontal){
 
-  }
-  public boolean takeShot(int x, int y){
+    }
+    public boolean takeShot(int x, int y){
 
-    return false;
-  }
-  public boolean isAllShipsSunk(){
+        return false;
+    }
+    public boolean isAllShipsSunk(){
 
-    return false;
-  }
-  public boolean isValidPlacement(Ship ship, int x, int y, boolean isHorizontal){
+        return false;
+    }
+    public boolean isValidPlacement(Ship ship, int x, int y, boolean isHorizontal){
 
-    return isHorizontal;
-  }
-  public boolean isGameOver(){
+        return isHorizontal;
+    }
+    public boolean isGameOver(){
 
-    return false;
-  }
+        return false;
+    }
 
 }
