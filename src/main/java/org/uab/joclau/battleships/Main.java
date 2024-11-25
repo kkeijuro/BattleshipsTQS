3d451bee6159ac42e7fd3cfad05ccfc97aaa3ed7
NIU01558589 <1558589@uab.cat>
2024-11-25 18:32:45 +0100

    checking restriction after bad checkstyle

--- src/main/java/org/uab/joclau/battleships/Main.java
+++ src/main/java/org/uab/joclau/battleships/Main.java
@@ -3,5 +3,8 @@ package org.uab.joclau.battleships;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+
+
 }
 
